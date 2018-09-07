33fe53ae6362fdadd72b31e861fbe5b936727db9
phantiensy <phantiensy195@gmail.com>
2018-09-07 16:42:16 +0700

    repository

--- src/main/java/com/banguoi/repository/ProductRepository.java
+++ src/main/java/com/banguoi/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.banguoi.repository;
 
 import com.banguoi.model.Product;
+import com.banguoi.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+    Page<Product> findProductByUser(User user, Pageable pageable);
 }
--- src/main/java/com/banguoi/repository/UserRepository.java
+++ src/main/java/com/banguoi/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Iterable<User> findUserByNameContaining(String name);
+    User findUserByEmail(String email);
 }
