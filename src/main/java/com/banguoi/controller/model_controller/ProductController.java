6e8f4a6b5ac73e42ee05d4d683df382a11d94d40
phantiensy <phantiensy195@gmail.com>
2018-09-11 22:36:03 +0700

    ProductController them chuc nang tim kiem product

--- src/main/java/com/banguoi/controller/model_controller/ProductController.java
+++ src/main/java/com/banguoi/controller/model_controller/ProductController.java
@@ -2,14 +2,17 @@ package com.banguoi.controller.model_controller;
 
 import com.banguoi.model.Product;
 import com.banguoi.model.Province;
+import com.banguoi.model.User;
 import com.banguoi.service.product.ProductService;
 import com.banguoi.service.province.ProvinceService;
+import com.banguoi.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,6 +25,9 @@ public class ProductController {
 
     @Autowired
     private ProvinceService provinceService;
+
+    @Autowired
+    private UserService userService;
 
     @ModelAttribute("provinces")
     public Iterable<Province> findAllProvince() {
@@ -41,18 +47,26 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public ModelAndView showListProduct(@RequestParam("product") Optional<String> product, Pageable pageable) {
+    public String showListProduct(@RequestParam("product") Optional<String> product,
+                                  @RequestParam("province") Optional<Province> province, Pageable pageable, ModelMap model) {
         Page<Product> products;
+
+        User user = userService.findUserByEmail(getPrincipal());
+        model.addAttribute("user", user);
+
         if (product.isPresent()) {
             products = productService.findAllByNameContaining(product.get(), pageable);
+            model.addAttribute("products", products);
+            return "/homestay/userSelected";
+        } else if (province.isPresent()) {
+            products = productService.findAllByProvince(province.get(), pageable);
+            model.addAttribute("products", products);
+            return "/homestay/userSelected";
         } else {
             products = productService.findAll(pageable);
+            model.addAttribute("products", products);
+            return "/product/list";
         }
-
-        ModelAndView modelAndView = new ModelAndView("/product/list");
-
-        modelAndView.addObject("products", products);
-        return modelAndView;
     }
 
     @GetMapping("/products/create")
