b497bc8a93c6f86e947f9687cd5558351bdfb3f4
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-09-13 11:45:04 +0700

    xoa truong roleService va ModelAttribute("role"

--- src/main/java/com/banguoi/controller/model_controller/UserController.java
+++ src/main/java/com/banguoi/controller/model_controller/UserController.java
@@ -1,9 +1,10 @@
 package com.banguoi.controller.model_controller;
 
-import com.banguoi.model.*;
+import com.banguoi.model.Product;
+import com.banguoi.model.Province;
+import com.banguoi.model.User;
 import com.banguoi.service.product.ProductService;
 import com.banguoi.service.province.ProvinceService;
-import com.banguoi.service.roles.RoleService;
 import com.banguoi.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,20 +12,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Optional;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private RoleService roleService;
 
     @Autowired
     private ProductService productService;
@@ -50,11 +45,6 @@ public class UserController {
         return email;
     }
 
-    @ModelAttribute("roles")
-    public Iterable<Role> listRoles() {
-        return roleService.findAll();
-    }
-
     @RequestMapping(value = "/user/manager", method = RequestMethod.GET)
     public ModelAndView listPostHomestay(Pageable pageable) {
         User user = userService.findUserByEmail(getPrincipal());
@@ -74,6 +64,7 @@ public class UserController {
     public ModelAndView showCreateHomestayForm() {
         User user = userService.findUserByEmail(getPrincipal());
         Product product = new Product();
+
         ModelAndView modelAndView = new ModelAndView("/homestay/create");
         modelAndView.addObject("product", product);
         modelAndView.addObject("user", user);
